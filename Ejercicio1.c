398218eead921343c2221fdcb85cc027e2247044
BRAM-25 <abram.zuni2@gmail.com>
2025-03-27 13:58:57 -0600

    Ejercicio1.c

---
+++ Ejercicio1.c
@@ -0,0 +1,22 @@
+// Abraham Zuñiga 
+// Ejercicio1
+//programa para validar si a es mayor a b.*/
+
+#include <stdio.h>
+
+void main()
+{
+    int a, b;
+    printf("dame un numero: ");
+    scanf("%i",&a);
+    printf("dame otro numero: ");
+    scanf("%i",&b);
+
+    if (a > b)
+    {
+        printf("a es mayor b \n");
+    }
+
+    printf("fin del programa \n");
+    
+} 
